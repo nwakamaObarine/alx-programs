42ebd7a9d32f300daf32be918ec8b7821f3f7e81
nwakamaObarine <arine.nwakama@gmail.com>
2023-01-05 02:27:22 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (s == '\0')
+	if (*s == '\0') /* base case*/
 	{
 		_putchar ('\n');
 		return;
 	}
-	_putcahr(*s);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
